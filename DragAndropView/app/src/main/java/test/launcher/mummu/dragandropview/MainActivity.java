f5e6fda5fbe7d692dcf6b21576f9f89f7758cb9b
muhammed <muhammed.thasneem@yahoo.com>
2016-07-25 14:15:30 +0530

    concept implemented

--- DragAndropView/app/src/main/java/test/launcher/mummu/dragandropview/MainActivity.java
+++ DragAndropView/app/src/main/java/test/launcher/mummu/dragandropview/MainActivity.java
@@ -1,13 +1,104 @@
 package test.launcher.mummu.dragandropview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, View.OnTouchListener, View.OnDragListener {
+    private ImageView dragImageView;
+    private String msg = "TAG";
+    private RelativeLayout.LayoutParams layoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dragImageView = (ImageView) findViewById(R.id.dragImageView);
+
+        dragImageView.setOnLongClickListener(this);
+
+        dragImageView.setOnDragListener(this);
+
+        dragImageView.setOnTouchListener(this);
     }
+
+    @Override
+    public boolean onLongClick(View v) {
+        ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+        String[] mimeType = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData clipData = new ClipData(v.getTag().toString(), mimeType, item);
+        View.DragShadowBuilder builder = new View.DragShadowBuilder();
+        v.startDrag(clipData, builder, null, 0);
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(dragImageView);
+
+            dragImageView.startDrag(data, shadowBuilder, dragImageView, 0);
+            dragImageView.setVisibility(View.INVISIBLE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                layoutParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+
+                // Do nothing
+                break;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
+                int x_cord = (int) event.getX();
+                int y_cord = (int) event.getY();
+                break;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
+                x_cord = (int) event.getX();
+                y_cord = (int) event.getY();
+                layoutParams.leftMargin = x_cord;
+                layoutParams.topMargin = y_cord;
+                v.setLayoutParams(layoutParams);
+                break;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
+                x_cord = (int) event.getX();
+                y_cord = (int) event.getY();
+                break;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
+
+                // Do nothing
+                break;
+
+            case DragEvent.ACTION_DROP:
+                Log.d(msg, "ACTION_DROP event");
+                // Do nothing
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
 }
